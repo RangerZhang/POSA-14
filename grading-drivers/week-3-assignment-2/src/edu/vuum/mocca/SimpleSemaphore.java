53f5a8b0eb83baaceb731335ddcee0e0bd713bc8
RangerZhang <rangerbin@gmail.com>
2014-06-12 01:09:58 +0800

    Peers Evaluation - Week3

--- grading-drivers/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
+++ grading-drivers/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -3,36 +3,49 @@ package edu.vuum.mocca;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.AbstractQueuedLongSynchronizer.ConditionObject;
 
 /**
  * @class SimpleSemaphore
  * 
- * @brief This class provides a simple counting semaphore implementation using
- *        Java a ReentrantLock and a ConditionObject. It must implement both
- *        "Fair" and "NonFair" semaphore semantics, just liked Java Semaphores.
+ * @brief This class provides a simple counting semaphore
+ *        implementation using Java a ReentrantLock and a
+ *        ConditionObject (which is accessed via a Condition). It must
+ *        implement both "Fair" and "NonFair" semaphore semantics,
+ *        just liked Java Semaphores.
  */
 public class SimpleSemaphore {
     /**
      * Define a ReentrantLock to protect the critical section.
      */
     // TODO - you fill in here
+	final private ReentrantLock mRL;
 
     /**
-     * Define a ConditionObject to wait while the number of
-     * permits is 0.
+     * Define a Condition that waits while the number of permits is 0.
      */
     // TODO - you fill in here
+	final private Condition mAvailableCond; 
 
     /**
      * Define a count of the number of available permits.
      */
     // TODO - you fill in here.  Make sure that this data member will
     // ensure its values aren't cached by multiple Threads..
+	volatile private int mAvailable;
 
     public SimpleSemaphore(int permits, boolean fair) {
         // TODO - you fill in here to initialize the SimpleSemaphore,
         // making sure to allow both fair and non-fair Semaphore
-        // semantics.
+        // semantics.    	   	
+    	
+    	if(true == fair){
+    		mRL = new ReentrantLock(true);
+    	}else {
+			mRL = new ReentrantLock(false);
+		}
+    	mAvailable = permits;
+    	mAvailableCond = mRL.newCondition(); 
     }
 
     /**
@@ -41,6 +54,20 @@ public class SimpleSemaphore {
      */
     public void acquire() throws InterruptedException {
         // TODO - you fill in here.
+    	try {
+    		mRL.lockInterruptibly();
+    		
+        	while (mAvailable <= 0) {			
+    			mAvailableCond.await();
+    		}
+        	
+        	mAvailable--;
+        	
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			mRL.unlock();
+		}    	
     }
 
     /**
@@ -49,6 +76,18 @@ public class SimpleSemaphore {
      */
     public void acquireUninterruptibly() {
         // TODO - you fill in here.
+    	try {
+    		mRL.lock();
+    		
+        	while (mAvailable <= 0) {			
+    			mAvailableCond.awaitUninterruptibly();;
+    		}
+        	
+        	mAvailable--;
+        	
+		} finally{
+			mRL.unlock();
+		} 
     }
 
     /**
@@ -56,6 +95,17 @@ public class SimpleSemaphore {
      */
     void release() {
         // TODO - you fill in here.
+    	try {
+    		mRL.lock();
+        	
+        	mAvailable++;
+        	mAvailableCond.signalAll();
+        	
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			mRL.unlock();
+		} 
     }
 
     /**
@@ -64,6 +114,6 @@ public class SimpleSemaphore {
     public int availablePermits() {
         // TODO - you fill in here by changing null to the appropriate
         // return value.
-        return null;
+        return mAvailable;
     }
 }
